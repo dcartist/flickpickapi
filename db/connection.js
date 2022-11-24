e399b3704df8aff7fecb0c84b1781bfe33b5c0f7
dcartist <pbannerman13@gmail.com>
2022-11-24 18:31:12 -0500

    update connection

--- db/connection.js
+++ db/connection.js
@@ -9,12 +9,21 @@ if (process.env.NODE_ENV === "production") {
 	mongoURI = 'mongodb://localhost/flickpick-api';
 }
 
-mongoose.connect(mongoURI, { useNewUrlParser: true })
-.then((conn) => {
-	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
-})
-.catch(err => {
-	console.error(err)
-});
+
+mongoose.connect(
+	process.env.DB_URL || 'mongodb://localhost/flickpick-api',
+	{
+	  useNewUrlParser: true,
+	  useUnifiedTopology: true,
+	},
+  );
+
+// mongoose.connect(mongoURI, { useNewUrlParser: true })
+// .then((conn) => {
+// 	console.log(`connected to mongodb on ${conn.connections[0].name} db`)
+// })
+// .catch(err => {
+// 	console.error(err)
+// });
 
 module.exports = mongoose
