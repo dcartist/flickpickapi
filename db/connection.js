9f4df0e521e56a3cbc0408106db13cc48177c7eb
dcartist <pbannerman13@gmail.com>
2022-11-25 09:06:00 -0500

    update connection

--- db/connection.js
+++ db/connection.js
@@ -1,23 +1,23 @@
 
-const mongoose = require('mongoose');
-mongoose.Promise = Promise;
+// const mongoose = require('mongoose');
+// mongoose.Promise = Promise;
 
-let mongoURI = '';
-if (process.env.NODE_ENV === "production") {
-	mongoURI = process.env.DB_URL;
-} else {
-	mongoURI = 'mongodb://localhost/flickpick-api';
-}
+// let mongoURI = '';
+// if (process.env.NODE_ENV === "production") {
+// 	mongoURI = process.env.DB_URL;
+// } else {
+// 	mongoURI = 'mongodb://localhost/flickpick-api';
+// }
 
 
-mongoose.connect(
-	process.env.DB_URL || 'mongodb://localhost/flickpick-api',
-	{
-		useNewUrlParser: true,
-		useFindAndModify: false,
-		useUnifiedTopology: true
-	},
-  );
+// mongoose.connect(
+// 	process.env.DB_URL || 'mongodb://localhost/flickpick-api',
+// 	{
+// 		useNewUrlParser: true,
+// 		useFindAndModify: false,
+// 		useUnifiedTopology: true
+// 	},
+//   );
 
 // mongoose.connect(mongoURI, { useNewUrlParser: true })
 // .then((conn) => {
@@ -26,5 +26,22 @@ mongoose.connect(
 // .catch(err => {
 // 	console.error(err)
 // });
+
+// module.exports = mongoose
+
+
+
+const mongoose = require('mongoose')
+mongoose.Promise = Promise
+let mongoURI = "";
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost:27017/flickpick-api";
+}
+mongoose
+    .connect(mongoURI, { useNewUrlParser: true })
+    .then(instance => console.log(`Connected to db: ${instance.connections[0].name}`)) //instance is what the database that your are connected to
+    .catch(error => console.log('Connection failed!', error))
 
 module.exports = mongoose
